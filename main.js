e62871314ea0c8d01ba112648a15d0cebe1def0e
budaman <budamantvydas@gmail.com>
2016-12-08 18:54:31 +0200

    global counting is added, everything is working

--- main.js
+++ main.js
@@ -8,10 +8,24 @@ var homeTeam = getHomeTeam.options[getHomeTeam.selectedIndex].value;
 
 
 
+var roadTeamPointsArray = [];
+var homeTeamPointsArray = [];
+var togetherPointsArray = [];
+var difArray = [];
 
-function test() {
+
+
+
+
+
+
+
+function countResults() {
+
+	alert (difArray.length);
 
 	//Getting a road team points
+
 	var totalRA = document.getElementById('totalRA').value;
 	var totalRD = document.getElementById('totalRD').value;
 	var last5RA = document.getElementById('last5RA').value;
@@ -30,21 +44,76 @@ function test() {
 	var restHD = document.getElementById('restHD').value;
 	
 	
+	//Counting formula
+
+	//==========ROAD TEAM =====================
 	var totalpointsRoad = totalRA * 2 + last5RA * 5 + restRA * 3 + totalHD * 2 + last5HD * 5 + restHD * 3  ;
 	var roadTeamPoints = totalpointsRoad / 20;
-	roadTeamPoints = Math.round(roadTeamPoints);
+	var roadTeamPoints = Math.round(roadTeamPoints);
+	roadTeamPointsArray.push(roadTeamPoints);
+
+	
+
+	
 
 
+
+	//=========HOME TEAM =======================
 	var totalpointsHome = totalRD * 2 + last5RD * 5 + restRD * 3 + totalHA * 2 + last5HA * 5 + restHA * 3  ;
 	var homeTeamPoints = totalpointsHome / 20;
-	homeTeamPoints = Math.round(homeTeamPoints);
+	var homeTeamPoints = Math.round(homeTeamPoints);
+	homeTeamPointsArray.push(homeTeamPoints);
+
+
+
+	//=========TOGETHER POINTS ===============
+	var togetherPoints = roadTeamPoints + homeTeamPoints;
+	togetherPointsArray.push(togetherPoints);
+
+
+
+	//==========DIFERENCE BETWEEN TEANS=========
+	var dif = roadTeamPoints - homeTeamPoints;
+	difArray.push(dif);
+
+
+	//HTML ELEMENTS APPEARING -----------------------
+	var container = document.getElementById('container');
+	var new_row = document.createElement('div');
+	new_row.className = "result";
+
+	var input = document.createElement("input");
+	input.type = "text";
+	input.size = 1;
+	input.id ="TeamRoadOdd"
+	input.className = "inputBet";
+
+	var input1 = document.createElement("input");
+	input1.type = "text";
+	input1.size = 1;
+	input1.id ="TeamHomeOdd"
+	input1.className = "inputBet";
+
+	 // put it into the DOM
+
+
+
+    
+    new_row.innerHTML = 	roadTeam + ": " + roadTeamPoints + " " + homeTeam + ": " + homeTeamPoints + " total: " + togetherPoints + " odd: " + dif + "</br> "
+    						+ "Bet company result: ";
+
+
+
+      container.appendChild(new_row);   
+      new_row.appendChild(input);     
+      new_row.appendChild(input1);   
 
 
 
 
 
 
-	alert(roadTeam + ": " + roadTeamPoints + " " + homeTeam + ": " + homeTeamPoints);
+
 	
 
 }
